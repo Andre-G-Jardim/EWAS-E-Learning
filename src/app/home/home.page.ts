c87fdea5ed9a17d3bfb4d84f1c48f767f34d6aca
Andre-G-Jardim <andrej@ufcspa.edu.br>
2021-02-13 18:02:25 -0300

    Desenvolvimento Tela Login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private navCtrl : NavController) {}
+  
+  ngOnInit(){}
+
+  page_login(){
+    this.navCtrl.navigateForward('login');
+  }
 
 }
