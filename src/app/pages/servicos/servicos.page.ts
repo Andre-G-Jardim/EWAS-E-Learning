f613cc01d95f18ccdca991cc52a50563d35ebc61
maicotc <triacamaico@gmail.com>
2021-02-27 21:31:19 -0300

    Layout Servicos e Maps (provisório)

--- src/app/pages/servicos/servicos.page.ts
+++ src/app/pages/servicos/servicos.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core'
+
+declare var google: any;
 
 @Component({
   selector: 'app-servicos',
   templateUrl: './servicos.page.html',
   styleUrls: ['./servicos.page.scss'],
 })
-export class ServicosPage implements OnInit {
+export class ServicosPage {
+  map: any;
+
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(-30.0277, -51.2287)
+    const startOptions = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, startOptions)
   }
 
 }
