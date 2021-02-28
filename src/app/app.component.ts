9306b705a2952c71160ccb6458af363866021838
maicotc <triacamaico@gmail.com>
2021-02-28 20:17:22 -0300

    Melhoria Maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Platform } from '@ionic/angular';
+
+import { Plugins, Capacitor } from '@capacitor/core';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
+      if (Capacitor.isPluginAvailable('SplashScreen')){
+        Plugins.SplashScreen.hide();
+      }
+    })
+  }
 }
--- src/app/pages/servicos/servicos.page.ts
+++ src/app/pages/servicos/servicos.page.ts
@@ -1,33 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core'
 
-declare var google: any;
+// declare var google: any;
 
 @Component({
   selector: 'app-servicos',
   templateUrl: './servicos.page.html',
   styleUrls: ['./servicos.page.scss'],
 })
-export class ServicosPage {
-  map: any;
+export class ServicosPage implements AfterViewInit {
+  // map: any;
 
-  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+  @ViewChild('map', { read: ElementRef, static: false }) mapElementRef: ElementRef;
+  location: string;
+  places: Array<any>;
 
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
-  ionViewDidEnter() {
-    this.showMap();
+  ngAfterViewInit() {
+    this.getGoogleMaps().then(googleMaps => {
+      const mapEl = this.mapElementRef.nativeElement;
+      const map = new googleMaps.Map(mapEl, {
+        //center: { lat: -30.0277, lng: -51.2287}, coords poa
+        center: { lat: -30.0313503, lng: -51.2212936 }, //coords ufcspa
+        zoom: 16
+      });
+      googleMaps.event.addListenerOnce(map, 'idle', () => {
+        this.renderer.addClass(mapEl, 'visible');
+      });
+
+
+    }).catch(err => {
+      console.log(err);
+    })
   }
 
-  showMap() {
-    const location = new google.maps.LatLng(-30.0277, -51.2287)
-    const startOptions = {
-      center: location,
-      zoom: 15,
-      disableDefaultUI: true
+  // ionViewDidEnter() {
+  //   this.showMap();
+  // }
+
+  // showMap() {
+  //   const location = new google.maps.LatLng(-30.0277, -51.2287)
+  //   const startOptions = {
+  //     center: location,
+  //     zoom: 15,
+  //     disableDefaultUI: true
+  //   }
+  //   this.map = new google.maps.Map(this.mapRef.nativeElement, startOptions)
+  // }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
     }
-    this.map = new google.maps.Map(this.mapRef.nativeElement, startOptions)
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAQv1v4rtu2UWvgV3L-YlRoyYF_NpMR554';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available! Check code')
+        }
+      }
+    })
   }
+
+
+  getLocation() {
+    alert(this.location);
+    // if (this.location === "farmacia") {
+    //   this.getPharmacies(Map.LatLng).then((results: Array<any>) => {
+    //     this.places = results;
+    //     for ()
+    //   })
+    // }
+  }
+
+  getPharmacies(latLng) {
+
+  }
+
 
 }
