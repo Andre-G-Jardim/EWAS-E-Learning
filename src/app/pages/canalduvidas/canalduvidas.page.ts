cd9475b840902449443b0c5b26899179f1adab4a
maicotc <triacamaico@gmail.com>
2021-03-13 10:41:16 -0300

    Enviar e-mail do canal de duvidas

--- src/app/pages/canalduvidas/canalduvidas.page.ts
+++ src/app/pages/canalduvidas/canalduvidas.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject,  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-canalduvidas',
   templateUrl: './canalduvidas.page.html',
   styleUrls: ['./canalduvidas.page.scss'],
 })
-export class CanalduvidasPage implements OnInit {
+export class CanalduvidasPage  {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private _document: Document
+  ) { }
 
-  ngOnInit() {
+  ionViewWillEnter(){
+
   }
 
+
+  public sendEmail(event: Event){
+    event.preventDefault();
+    emailjs.sendForm(environment.emailjs.serviceID, 
+    environment.emailjs.templateID, 
+    event.target as HTMLFormElement, 
+    environment.emailjs.userID).then((result: EmailJSResponseStatus) => {
+      console.log(result.text);
+      this._document.defaultView.location.reload()
+    }, (error) => {
+      console.log(error.text);
+    });
+  }
+
+  refreshPage(){
+    this._document.defaultView.location.reload();
+  }
 }
