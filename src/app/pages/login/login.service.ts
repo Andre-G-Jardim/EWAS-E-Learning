8c9159b1a31a90176f6feb137c12af26e8457dda
meuplow <gabrielgm@ufcspa.edu.br>
2021-03-16 20:49:50 -0300

    recuperando e exibindo nome-email do user no menu

--- src/app/pages/login/login.service.ts
+++ src/app/pages/login/login.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { User } from 'app/interface/user';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,15 @@ export class LoginService {
 
   private _usuarioAutenticado = false;
 
+  private userCollection: AngularFirestoreCollection<User>;
+
   get usuarioAutenticado(){
     return this._usuarioAutenticado;
   }
 
-  constructor(private afa: AngularFireAuth) { }
+  constructor(private afa: AngularFireAuth, private afs: AngularFirestore) {
+    this.userCollection = this.afs.collection<User>('Usuarios');
+  }
 
   logado(){
     this._usuarioAutenticado = true;
@@ -38,6 +43,18 @@ export class LoginService {
 
   redefinirSenha(usuario: User){
     return this.afa.sendPasswordResetEmail(usuario.email);
+  }
+
+  getIdUser(id: string){
+    return this.userCollection.doc<User>(id).snapshotChanges().pipe(
+      map(a => {
+        if(a.payload.exists){
+          const data = a.payload.data();
+          const payloadId = a.payload.id;
+          return {id: payloadId, ...data};
+        }
+      })
+    )
   }
 
 }
