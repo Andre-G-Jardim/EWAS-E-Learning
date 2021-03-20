3a4978581aad702e3db7a8487567149d8d0ad747
Andre-G-Jardim <andrej@ufcspa.edu.br>
2021-03-20 17:41:45 -0300

    notificaçoes

--- src/app/pages/metodos/metodos.page.ts
+++ src/app/pages/metodos/metodos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Metodo } from 'app/interface/metodo';
 import { Subscription } from 'rxjs';
 import { MetodosConteudoService } from '../metodos-conteudo/metodos-conteudo.service';
@@ -11,12 +12,36 @@ import { MetodosConteudoService } from '../metodos-conteudo/metodos-conteudo.ser
 export class MetodosPage implements OnInit {
   public metodos = new Array<Metodo>();
   private metodosSubscription: Subscription;
+  private headerToast : string[] = [
+    'Você sabe qual a efetividade da pílula oral combinada?', 
+    'Está com alguma dúvida?', 
+    'Você sabe a diferença da Minipílula para a Oral Combinada?',
+    'Já ouviu falar do implante hormonal?', 
+    'Você sabe quando o anticomcepcional injetável não é indicado?',
+    'Você sabe o tempo de duração de um DIU?'
+    ];
+    private messageToast : string[] = [
+    'Confira na aba Materias', 
+    'Entre em contato conosco pelo nosso canal de dúvidas!',
+    'Confira nos Métodos contraceptivos',
+    'Ele é um dos mais efetivos',
+    'Confira nos Materiais de métodos contraceptivos',
+    'Confira na aba DIU nos métodos contraceptivos'
+    ];
+    private index = 0;
 
-  constructor(private metodosService: MetodosConteudoService) {
+  constructor(
+    private metodosService: MetodosConteudoService, 
+    private toastCtrl: ToastController) {
     this.metodosSubscription = this.metodosService.getMetodos().subscribe(data => {
       this.metodos = data;
     })
   }
+
+  ionViewDidEnter() {
+    this.index = this.randomInt(0, (this.headerToast.length-1))
+    this.toastHome();
+   }
 
   ngOnInit() {
   }
@@ -25,5 +50,27 @@ export class MetodosPage implements OnInit {
     this.metodosSubscription.unsubscribe();
   }
 
+  async toastHome(){
+
+    try {
+      this.toastCtrl.dismiss();
+    } catch(e) {}
+    let toast = await this.toastCtrl.create({ 
+      animated: true,
+      header: this.headerToast[this.index],
+      message: this.messageToast[this.index],
+      cssClass: 'toast-Custon-Class round',
+      mode: 'ios',
+      duration: 5000,
+      buttons: [{icon:'notifications'}]
+      
+    });
+
+    toast.present();
+  }
+
+  public randomInt = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
 }
--- src/app/pages/videos/videos.page.ts
+++ src/app/pages/videos/videos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Videos } from 'app/interface/videos';
 import { Subscription } from 'rxjs';
 import { VideosTemaService } from '../videos-tema/videos-tema.service';
@@ -11,12 +12,37 @@ import { VideosTemaService } from '../videos-tema/videos-tema.service';
 export class VideosPage implements OnInit {
   public videos = new Array<Videos>();
   private videosSubscription: Subscription;
+  private headerToast : string[] = [
+    'Você sabe qual a efetividade da pílula oral combinada?', 
+    'Está com alguma dúvida?', 
+    'Você sabe a diferença da Minipílula para a Oral Combinada?',
+    'Já ouviu falar do implante hormonal?', 
+    'Você sabe quando o anticomcepcional injetável não é indicado?',
+    'Você sabe o tempo de duração de um DIU?'
+    ];
+    private messageToast : string[] = [
+    'Confira na aba Materias', 
+    'Entre em contato conosco pelo nosso canal de dúvidas!',
+    'Confira nos Métodos contraceptivos',
+    'Ele é um dos mais efetivos',
+    'Confira nos Materiais de métodos contraceptivos',
+    'Confira na aba DIU nos métodos contraceptivos'
+    ];
+    private index = 0;
 
-  constructor(private videoService: VideosTemaService) {
+
+  constructor(
+    private videoService: VideosTemaService,
+    private toastCtrl: ToastController) {
     this.videosSubscription = this.videoService.getVideos().subscribe(data => {
       this.videos = data;
     })
   }
+
+  ionViewDidEnter() {
+    this.index = this.randomInt(0, (this.headerToast.length-1))
+    this.toastHome();
+   }
 
   ngOnInit() {
   }
@@ -24,5 +50,28 @@ export class VideosPage implements OnInit {
   ngOnDestroy(){
     this.videosSubscription.unsubscribe();
   }
+
+  async toastHome(){
+
+    try {
+      this.toastCtrl.dismiss();
+    } catch(e) {}
+    let toast = await this.toastCtrl.create({ 
+      animated: true,
+      header: this.headerToast[this.index],
+      message: this.messageToast[this.index],
+      cssClass: 'toast-Custon-Class round',
+      mode: 'ios',
+      duration: 5000,
+      buttons: [{icon:'notifications'}]
+      
+    });
+
+    toast.present();
+  }
+
+  public randomInt = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
 }
