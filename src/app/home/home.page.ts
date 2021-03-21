a5b2c3f6caaca36652e01f05379772c9bf64d592
meuplow <gabrielgm@ufcspa.edu.br>
2021-03-21 18:52:02 -0300

    alteracao homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,38 +8,8 @@ import { LoginService } from 'app/pages/login/login.service';
 })
 export class HomePage implements OnInit{
 
-  deferredPrompt;
-
   constructor() {}
 
   ngOnInit(){}
-
-  ionViewWillEnter() {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      console.log('beforeinstallprompt Event fired');
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-    });
-  }
-
-  showInstallBanner() {
-    if (this.deferredPrompt !== undefined && this.deferredPrompt !== null) {
-      // Show the prompt
-      this.deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      this.deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        // We no longer need the prompt.  Clear it up.
-        this.deferredPrompt = null;
-      });
-    }
-  }
 
 }
